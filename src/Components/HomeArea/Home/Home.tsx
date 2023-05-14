ec43675396ca1353b41f5a894c309e3d522d2f2d
ruthiv <rvengrover@gmail.com>
2023-05-14 23:34:12 +0300

    מחיקת הערה

--- src/Components/HomeArea/Home/Home.tsx
+++ src/Components/HomeArea/Home/Home.tsx
@@ -6,79 +6,77 @@ import React from "react";
 
 
 interface TabPanelProps {
-    children?: React.ReactNode;
-    index: number;
-    value: number;
-  }
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
 
-  // gghdhedhdr
-  function TabPanel(props: TabPanelProps) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`vertical-tabpanel-${index}`}
-        aria-labelledby={`vertical-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-  }
-  
-
-  function a11yProps(index: number) {
-    return {
-      id: `vertical-tab-${index}`,
-      'aria-controls': `vertical-tabpanel-${index}`,
-    };
-  }
-function Home(): JSX.Element {
-    const [value, setValue] = React.useState(0);
-
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-        setValue(newValue);
-      };
-    
-    return (
-        <div className="Home">
-                {/* <img src={bus}/> */}
-
-	<div className="box">         
-       <Box
-      sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 180  ,flexDirection: 'row-reverse'}}
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
     >
-      <Tabs
-        orientation="vertical"
-        // variant="scrollable"
-        value={value}
-        onChange={handleChange}
-        aria-label="Vertical tabs example"
-        sx={{ borderLeft: 1, borderColor: 'divider' }}
-      >
-        <Tab label="הנהלה" {...a11yProps(0)} sx={{ fontSize: 20 }}/>
-        <Tab label="מורות" {...a11yProps(1)} sx={{ fontSize: 20 }}/>
-        <Tab label="הורים" {...a11yProps(2)} sx={{ fontSize: 20 }}/>
-        
-      </Tabs>
-      <TabPanel value={value} index={0}>
-       הנהלה
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        מורות
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        הורים
-      </TabPanel>
-      </Box></div>	
-        </div>
-    );
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+
+function a11yProps(index: number) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+function Home(): JSX.Element {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className="Home">
+      {/* <img src={bus}/> */}
+
+      <div className="box">
+        <Box
+          sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 180, flexDirection: 'row-reverse' }}
+        >
+          <Tabs
+            orientation="vertical"
+            // variant="scrollable"
+            value={value}
+            onChange={handleChange}
+            aria-label="Vertical tabs example"
+            sx={{ borderLeft: 1, borderColor: 'divider' }}
+          >
+            <Tab label="הנהלה" {...a11yProps(0)} sx={{ fontSize: 20 }} />
+            <Tab label="מורות" {...a11yProps(1)} sx={{ fontSize: 20 }} />
+            <Tab label="הורים" {...a11yProps(2)} sx={{ fontSize: 20 }} />
+
+          </Tabs>
+          <TabPanel value={value} index={0}>
+            הנהלה
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            מורות
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            הורים
+          </TabPanel>
+        </Box></div>
+    </div>
+  );
 }
 
 export default Home;
