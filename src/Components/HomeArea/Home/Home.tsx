87c685843aff21286a89380533cba97ce3424b24
ruthiv <rvengrover@gmail.com>
2023-05-16 22:04:05 +0300

    update

--- src/Components/HomeArea/Home/Home.tsx
+++ src/Components/HomeArea/Home/Home.tsx
@@ -20,7 +20,7 @@ function TabPanel(props: TabPanelProps) {
             role="tabpanel"
             hidden={value !== index}
             id={`vertical-tabpanel-${index}`}
-            aria-labelledby={ `vertical-tab-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
             {...other}
         >
             {value === index && (
@@ -46,25 +46,25 @@ function Home(): JSX.Element {
     };
     return (
         <div className="Home">
-{/* <img src={bus}/> */}
-  <div className="box">
-  <Box sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 180  ,flexDirection: 'row-reverse'}}    >      <Tabs orientation="vertical"        // variant="scrollable"  
-  value={value}
-   onChange={handleChange}
-    aria-label="Vertical tabs example"
-     sx={{ borderLeft: 1, borderColor: 'divider' }} >
-       <Tab label="הנהלה" {...a11yProps(0)} sx={{ fontSize: 20 }}/>
-        <Tab label="מורות" {...a11yProps(1)} sx={{ fontSize: 20 }}/>
-        <Tab label="הורים" {...a11yProps(2)} sx={{ fontSize: 20 }}/>
-         </Tabs>
-          <TabPanel value={value} index={0}>
-         <SchoolLogin/>
-          </TabPanel>
-          <TabPanel value={value} index={1}> <TeacherLogin/></TabPanel>
-           <TabPanel value={value} index={2}> הורים </TabPanel>
-            </Box>
-           </div> 
-           </div>
+            {/* <img src={bus}/> */}
+            <div className="box">
+                <Box sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 180, flexDirection: 'row-reverse' }}    >      <Tabs orientation="vertical"        // variant="scrollable"  
+                    value={value}
+                    onChange={handleChange}
+                    aria-label="Vertical tabs example"
+                    sx={{ borderLeft: 1, borderColor: 'divider' }} >
+                    <Tab label="הנהלה" {...a11yProps(0)} sx={{ fontSize: 20 }} />
+                    <Tab label="מורות" {...a11yProps(1)} sx={{ fontSize: 20 }} />
+                    <Tab label="הורים" {...a11yProps(2)} sx={{ fontSize: 20 }} />
+                </Tabs>
+                    <TabPanel value={value} index={0}>
+                        <SchoolLogin />
+                    </TabPanel>
+                    <TabPanel value={value} index={1}> <TeacherLogin /></TabPanel>
+                    <TabPanel value={value} index={2}> הורים </TabPanel>
+                </Box>
+            </div>
+        </div>
     );
 }
 
