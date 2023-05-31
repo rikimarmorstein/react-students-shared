fd819f4412992adee63810ec233384eb01d502d5
ruthiv <rvengrover@gmail.com>
2023-05-31 10:34:39 +0300

    update routing

--- src/Components/LayoutArea/Routing/Routing.tsx
+++ src/Components/LayoutArea/Routing/Routing.tsx
@@ -8,6 +8,9 @@ import PageNotFound from "../PageNotFount/PageNotFound";
 import TeacherDetails from "../../Teacher/TeacherDetails/TeacherDetails";
 import AddStudent from "../../SchoolDirectorArea/AddStudent/AddStudent";
 import SchoolDirectorDetails from "../../SchoolDirectorArea/SchoolDirectorDetails/SchoolDirectorDetails";
+import UpdateTeacher from "../../SchoolDirectorArea/UpdateTeacher/UpdateTeacher";
+import DeleteTeacher from "../../SchoolDirectorArea/DeleteTeacher/DeleteTeacher";
+import TeacherList from "../../SchoolDirectorArea/TeacherList/TeacherList";
 
 function Routing(): JSX.Element {
 
@@ -23,12 +26,23 @@ function Routing(): JSX.Element {
                 <Route path="/admin-login" element={<AdminArea />} />
 //school
                 <Route path="/login" element={<Login />} />
-                <Route path="/school-director/add-teacher" element={<AddTeacher />} />
                 <Route path="/school-director" element={<SchoolDirectorDetails />} />
+
+//teacher in SchoolDirector
+                <Route path="/school-director/add-teacher" element={<AddTeacher />} />
+                <Route path="/school-director/update-teacher" element={<UpdateTeacher />} />
+                <Route path="/school-director/delete-teacher" element={<DeleteTeacher />} />
+                <Route path="/school-director/list-teacher" element={<TeacherList />} />
+                {/* <Route path="/school-director/card-teacher" element={<TeacherCard />} /> */}
+
+//student in SchoolDirector
                 <Route path="/add-student" element={<AddStudent />} />
 
+//teacher
                 <Route path="/teacher" element={<TeacherDetails />} />
+//student                
                 {/* <Route path="/student" element={<StudentDetails />} />
+ //transportation              
                 <Route path="/transportation" element={<TransportationDetails />} /> */}
 
                 <Route path="*" element={<PageNotFound />} />
